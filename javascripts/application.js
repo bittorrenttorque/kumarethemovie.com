6a95640d3cf713039585c584b59914ba85b873bb
Patrick Williams <patrick@bittorrent.com>
2012-06-19 10:40:42 -0700

    only loading images when they are fully downloaded

--- javascripts/application.js
+++ javascripts/application.js
@@ -12,7 +12,16 @@ jQuery(function() {
 	var name = escape('Kumare - Exclusive Movie Clip_(mkv h.264 720p).mkv');
 	btapp.live(
 		'torrent ' + hash + ' file ' + name + ' properties streaming_url', 
-		function(src) {
+		function(src, properties, file, files, torrent, torrents) {
+			// Lets show a nice progress bar...torrents don't download sequentially,
+			// so its nice to have some other way of knowing that its actually downloading.
+			var update_progress = function() {
+				var progress = 100.0 * properties.get('downloaded') / properties.get('size');
+				$('.progress .bar').css('width', progress + '%');
+			};
+			update_progress();
+			properties.on('change:downloaded', update_progress);
+
 			_V_("kumare_video").ready(function(){
 				this.src(src);
 				this.play();
@@ -23,12 +32,23 @@ jQuery(function() {
 	// Lets display the images in the torrent
 	btapp.live(
 		'torrent ' + hash + ' file * properties',
-		function(properties) {
-			var name = properties.get('name');
-			var src = properties.get('streaming_url');
-			if(name.indexOf('jpg', name.length - 3) !== -1) {
-				var img = $('<img src="' + src + '" />');
-				$('#kumare_images').append(img);
+		function(properties, file, files, torrent, torrents) {
+			// Lets not show the images immediately. If they're not downloaded yet
+			// they will be displayed as dead links...very sad indeed.
+			var show_image = function() {
+				if(properties.get('downloaded') === properties.get('size')) {
+					var name = properties.get('name');
+					var src = properties.get('streaming_url');
+					if(name.indexOf('jpg', name.length - 3) !== -1) {
+						var img = $('<img src="' + src + '" />');
+						$('#kumare_images').append(img);
+					}
+				}
+			};
+			if(properties.get('downloaded') === properties.get('size')) {
+				show_image();
+			} else {
+				properties.on('change:downloaded', show_image);
 			}
 		}
 	);
